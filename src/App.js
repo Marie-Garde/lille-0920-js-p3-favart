f316c8da7b6e857d7dabf1d87f212a6822d7af68
Fx-Colliot <fxcolliot@gmail.com>
2020-12-14 18:34:45 +0100

    Dowload npm reset and import reset tag in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from "./Components/Header";
+import { Reset } from "styled-reset";
 
 export default function App() {
   return (
     <div>
+      <Reset />
       <Header />
     </div>
   );
