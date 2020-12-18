647c5fde308fb0c4c34738be25df82f7b842ca58
Fx-Colliot <fxcolliot@gmail.com>
2020-12-19 00:25:23 +0100

    Delete styled const transferred to the new component HeaderTopStyled

--- src/Styled-components/HeaderStyled.jsx
+++ src/Styled-components/HeaderStyled.jsx
@@ -1,44 +1,5 @@
 import styled from "styled-components";
 
-//Logo, network-link, connexion
-
-export const DivHeaderTop = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-export const ContainerLogo = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-export const Logo = styled.img`
-  height: 15vh;
-  width: 25vw;
-`;
-
-export const IconTwitter = styled.img`
-  width: 2.5vw;
-`;
-export const IconLinkedin = styled.img`
-  width: 2.5vw;
-  padding-left: 1vw;
-  padding-right: 1vw;
-`;
-
-export const IconMail = styled.img`
-  width: 2.5vw;
-`;
-
-export const ContainerConnexion = styled.div`
-  display: inline;
-  align-items: center;
-  justify-content: center;
-  font-size: 2.5rem;
-  font-family: "Roboto", sans-serif;
-`;
-
 //Nav2
 
 export const ContainerNav2 = styled.div`
