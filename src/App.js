4ec73ab5533d44b246ab25eecb58c8c4ff4eca52
Fx-Colliot <fxcolliot@gmail.com>
2020-12-19 17:10:54 +0100

    Rename component Header to HeaderBody

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Header from "./Components/Header";
+import HeaderBody from "./Components/HeaderBody";
 import { Reset } from "styled-reset";
 
 export default function App() {
   return (
     <div>
       <Reset />
-      <Header />
+      <HeaderBody />
     </div>
   );
 }
