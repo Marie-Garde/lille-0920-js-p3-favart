8f50fdc79a6a220bc1fd0d26e3ec5037b38f4363
Fx-Colliot <fxcolliot@gmail.com>
2020-12-19 16:02:59 +0100

    Adding HeaderMenu in App.js, transfer informations for the component complete on header branch to header-menu branch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import HeaderMenu from "./components/HeaderMenu";
 import Mainpage from "./components/Main";
 
 export default function App() {
   return (
     <div>
+      <HeaderMenu />
       <Mainpage />
     </div>
   );
---
+++ src/Styled-components/HeaderMenuStyled.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const ContainerHeaderMenu = styled.div`
+  display: flex;
+  align-items: center;
+  height: 15vh;
+  width: 100%;
+  background-color: #1f89d1;
+`;
+
+export const ContainerHeaderMenuLink = styled.div`
+  margin-left: 15vw;
+`;
+
+export const MenuLink = styled.a`
+  font-size: 3rem;
+  color: #0e273d;
+  text-decoration: none;
+`;
