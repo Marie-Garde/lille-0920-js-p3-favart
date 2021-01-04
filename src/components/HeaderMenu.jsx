89480f7be2f7827d7217355be3315d581e5b00ad
Fx-Colliot <fxcolliot@gmail.com>
2021-01-04 10:25:27 +0100

    Delete the first Div tag

--- src/components/HeaderMenu.jsx
+++ src/components/HeaderMenu.jsx
@@ -9,16 +9,14 @@ import {
 export default function HeaderMenu() {
   return (
     <>
-      <>
-        <ContainerHeaderMenu>
-          <ContainerHeaderMenuLink>
-            <MenuLink href="">01 - Favart </MenuLink>
-            <MenuLink href="">/ 02 /</MenuLink>
-            <MenuLink href=""> 03 </MenuLink>
-            <MenuLink href="">/ 04</MenuLink>
-          </ContainerHeaderMenuLink>
-        </ContainerHeaderMenu>
-      </>
+      <ContainerHeaderMenu>
+        <ContainerHeaderMenuLink>
+          <MenuLink href="">01 - Favart </MenuLink>
+          <MenuLink href="">/ 02 /</MenuLink>
+          <MenuLink href=""> 03 </MenuLink>
+          <MenuLink href="">/ 04</MenuLink>
+        </ContainerHeaderMenuLink>
+      </ContainerHeaderMenu>
     </>
   );
 }
