008eeb064e9534bdaf4536cb9ad7ab560de23ac2
Marie Garde <marie_garde@ymail.com>
2021-01-05 15:17:13 +0100

    Text added

--- src/Styled-components/Agissons.jsx
+++ src/Styled-components/Agissons.jsx
@@ -3,13 +3,33 @@ import { device } from "../Assets/Device/Device";
 
 export const Card = styled.div`
   background-color: red;
+  width: 50vw;
+  height: 40vh;
   display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin-top: 5vh;
+  margin-left: 5vh;
 `;
 
-export const CardText = styled.h1`
+export const CardText = styled.div`
+  width: 30vw;
+  margin-right: 20vw;
+`;
+
+export const Title = styled.h1`
+  font-size: 1.4;
+`;
+
+export const Text = styled.h2`
+  font-size: 1.3;
+`;
+
+export const Liste = styled.ul`
   font-size: 1.4;
 `;
 
 export const CardImg = styled.img`
-  height: 10vh;
+  width: 30vw;
+  margin-left: -10vw;
 `;
--- src/components/Agissons.jsx
+++ src/components/Agissons.jsx
@@ -1,13 +1,43 @@
-import { notfound } from "../Assets/imagenotfound.jpeg";
+import notfound from "../Assets/imagenotfound.jpeg";
 
-import { Card, CardText, CardImg } from "../Styled-components/Agissons.jsx";
+import {
+  Card,
+  CardText,
+  CardImg,
+  Title,
+  Text,
+  Liste,
+} from "../Styled-components/Agissons.jsx";
 
 export default function Agissons() {
   return (
     <>
       <Card>
-        <CardText>Blabla</CardText>
-        <CardImg src="notfound"></CardImg>
+        <CardText>
+          <Title>
+            Droit à l’expérimentation : MOBILISER le droit à l’expérimentation
+            territoriale
+          </Title>
+          <Text>
+            pour lever les freins réglementaires associés à la mise en œuvre des
+            solutions à impact et changer les règles du jeu (réglementaire,
+            économique, juridique et financier). Favart, pionnier du droit à
+            l’expérimentation, à travers une méthodologie qui permet :
+          </Text>
+          <Liste>
+            <li>
+              Un cadre juridique et administratif maîtrisé pour les porteurs de
+              projets et les collectivités territoriales en lien avec le projet
+              de loi 3D (Décentralisation, Différentiation, Déconcentration)
+            </li>
+            <li>
+              Une implication ministérielle favorable aux expérimentations
+            </li>
+            <li>Un délai suffisant pour dégager des résultats probants</li>
+            <li>Une évaluation pour définir les critères de succès </li>
+          </Liste>
+        </CardText>
+        <CardImg src={notfound}></CardImg>
       </Card>
     </>
   );
