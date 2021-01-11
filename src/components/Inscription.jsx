ff1a7fe63c8b4a6712b0f5d0f250f39bb530f627
Marie Garde <marie_garde@ymail.com>
2021-01-11 09:52:46 +0100

    First questions ok

--- src/components/Inscription.jsx
+++ src/components/Inscription.jsx
@@ -1,3 +1,69 @@
-import { H1Left } from "../styled-components/Inscription";
+import {
+  FlexBox,
+  FlexBox1,
+  FlexBox2,
+  P,
+  Formulaire,
+  DivLabel,
+  Label,
+  Input,
+  StyledButton,
+} from "../styled-components/Inscription";
 
-export default function Inscription() {}
+export default function Inscription() {
+  return (
+    <FlexBox>
+      <FlexBox1></FlexBox1>
+      <FlexBox2>
+        <P>
+          Avant d’accéder à l’espace client, veuillez renseigner ces champs :
+        </P>
+        <Formulaire>
+          <fieldset>
+            <DivLabel>
+              <Label>Lastname *</Label>
+              <Input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Lastname"
+                required
+              />
+            </DivLabel>
+            <DivLabel>
+              <Label>Firstname *</Label>
+              <Input
+                type="text"
+                name="firstname"
+                id="firstname"
+                placeholder="Firstname"
+                required
+              />
+            </DivLabel>
+            <DivLabel>
+              <Label>Métier *</Label>
+              <Input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email"
+                required
+              />
+            </DivLabel>
+            <DivLabel>
+              <Label>Nom de la structure *</Label>
+              <Input
+                type="text"
+                name="Structure name"
+                id="Structurename"
+                placeholder="Nom de la structure"
+                required
+              />
+            </DivLabel>
+            <StyledButton type="submit" value="Send" />
+          </fieldset>
+        </Formulaire>
+      </FlexBox2>
+    </FlexBox>
+  );
+}
---
+++ src/styled-components/Inscription.jsx
@@ -0,0 +1,118 @@
+import styled from "styled-components";
+import { Device } from "../assets/Device/Device";
+
+export const FlexBox = styled.div`
+  display: flex;
+  @media ${Device.mobile} {
+    flex-direction: column;
+  }
+`;
+
+export const FlexBox1 = styled.div`
+  flex: 1;
+  background: rgb(35, 134, 201);
+  background: linear-gradient(
+    180deg,
+    rgba(35, 134, 201, 1) 0%,
+    rgba(18, 34, 57, 1) 100%
+  );
+  height: 100vh;
+  @media ${Device.mobile} {
+    flex-direction: column;
+    height: 5vh;
+  }
+`;
+
+export const FlexBox2 = styled.div`
+  flex: 2;
+  height: 100vh;
+  padding-right: 3vw;
+  padding-left: 3vw;
+  padding-top: 8vh;
+`;
+
+export const P = styled.h1`
+  font-size: 1.5rem;
+  font-family: "Josefin Sans";
+`;
+
+export const Formulaire = styled.form`
+  width: 65vw;
+  float: right;
+  margin-top: 20vh;
+  margin-right: 2vw;
+  display: flex;
+  font-size: 1.5em;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 50vh;
+  @media ${Device.mobile} {
+    height: 40vh;
+    width: 40vh;
+    margin-right: 35vw;
+  }
+`;
+
+export const DivLabel = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 5vh;
+  width: 100%;
+  text-align: start;
+  height: 2.7vh;
+  @media ${Device.mobile} {
+  }
+`;
+
+export const DivTheme = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 5vh;
+  width: 100%;
+  text-align: start;
+  height: 2.7vh;
+  @media ${Device.mobile} {
+  }
+`;
+
+export const Label = styled.label`
+  color: white;
+  text-shadow: 1px 1px 2px black;
+  display: inline-block;
+  margin-right: 2vw;
+  width: 30%;
+  @media ${Device.mobile} {
+    display: none;
+  }
+`;
+
+export const Input = styled.input`
+  width: 40%;
+  text-align: center;
+  @media ${Device.mobile} {
+    width: 70vw;
+  }
+`;
+
+export const StyledButton = styled.input`
+  padding: 0 20px;
+  border: none;
+  background: #370d53;
+  color: #c4c1c7;
+  letter-spacing: 2px;
+  transition: 0.2s all ease-in-out;
+  border-bottom: 2px solid transparent;
+  outline: none;
+  height: 4vh;
+  border-radius: 5px;
+  margin-top: 10vh;
+  &:hover {
+    background: #51218f;
+    color: #d7d0db;
+    cursor: pointer;
+  }
+  @media ${Device.mobile} {
+    margin-top: 10vh;
+    margin-left: 15vw;
+  }
+`;
