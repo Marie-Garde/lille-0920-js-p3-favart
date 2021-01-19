15ac40540adec854d9757a46d9610df094deacd9
Thoms-C <61702462+Thoms-C@users.noreply.github.com>
2021-01-19 15:47:44 +0100

    style for grid

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -86,86 +86,115 @@ export default function Grid() {
                   </LiList>
                 </Test>
               </ContainerUl1>
-              {/*<ContainerUl2>
-                <ul>
-                  <li>Groupement Oxalia (bailleurs sociaux)</li>
-                  <li>
-                    Prévenir les sorties sèches à la fin des dispositifs
-                    institutionnels et permettre l’accès au l’autonomie
-                    citoyenne des jeunes
-                  </li>
-                  <li>
-                    Hauts-de-France, Valenciennois, Métropole Européenne de
-                    Lille, Maubeugeois-Avesnois
-                  </li>
-                  <li>
-                    Mise en œuvre d’une nouvelle stratégie régionale pour lutter
-                    contre les sorties sèches à la fin de l’aide sociale à
-                    l’enfance (ASE)
-                  </li>
-                  <li>
-                    <ul>
-                      <li>Droit à l’expérimentation</li>
-                      <li>Communauté d’action</li>
-                      <li>Modèle opérationnel</li>
-                      <li>Modèle économique à impact</li>
-                      <li>Modèle juridique et financier</li>
-                    </ul>
-                  </li>
-                </ul>
-              </ContainerUl2>*/}
             </BlockUl>
-            <BlockDescription>
-              {/*<Description>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum
-                alias voluptates fugiat modi eveniet rerum vitae distinctio
-                laudantium porro labore dolore illum sapiente temporibus eius
-                nulla in, tenetur odio. Rerum.
-              </Description>*/}
-            </BlockDescription>
           </ContainerBlock>
         </Grid1>
         <Grid2>
-          <Title>Block2</Title>
-          <Details2>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum alias
-            voluptates fugiat modi eveniet rerum vitae distinctio laudantium
-            porro labore dolore illum sapiente temporibus eius nulla in, tenetur
-            odio. Rerum.
-          </Details2>
+          <BlockLogoTitle>
+            <LogoProjet src={oxalia} alt="image projet" />
+            <Title>Programme D’abord Jeune</Title>
+          </BlockLogoTitle>
+          <ContainerBlock>
+            <BlockUl>
+              <ContainerUl1>
+                <Test>
+                  <LiList>
+                    <Span>Porteur du projet :</Span>
+                    <p>Groupement Oxalia (bailleurs sociaux)</p>
+                  </LiList>
+                  <LiList>
+                  <Span>Enjeux :</Span>
+                    <p>
+                      Prévenir les sorties sèches à la fin des dispositifs
+                      institutionnels et permettre l’accès au l’autonomie
+                      citoyenne des jeunes
+                    </p>
+                  </LiList>
+                  <LiList>
+                  <Span>Territoires :</Span>
+                    <p>
+                      Hauts-de-France, Valenciennois, Métropole Européenne de
+                      LiListlle, Maubeugeois-Avesnois
+                    </p>
+                  </LiList>
+                  <LiList>
+                  <Span>Mission Favart :</Span>
+                    <p>
+                      Mise en œuvre d’une nouvelle stratégie régionale pour
+                      lutter contre les sorties sèches à la fin de l’aide
+                      sociale à l’enfance (ASE)
+                    </p>
+                  </LiList>
+                  <LiList>
+                    <OutilsContainer>
+                    <Span>Outils Favart :</Span>
+                      <Center>
+                        <Tools>Droit à l’expérimentation</Tools>
+                        <Tools>Communauté d’action</Tools>
+                        <Tools>Modèle opérationnel</Tools>
+                        <Tools>Modèle économique à impact</Tools>
+                        <Tools>Modèle juridique et financier</Tools>
+                      </Center>
+                    </OutilsContainer>
+                  </LiList>
+                </Test>
+              </ContainerUl1>
+            </BlockUl>
+          </ContainerBlock>
         </Grid2>
-
         <Grid3>
-          <Title>Block3</Title>
-          <Details3>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum alias
-            voluptates fugiat modi eveniet rerum vitae distinctio laudantium
-            porro labore dolore illum sapiente temporibus eius nulla in, tenetur
-            odio. Rerum.
-          </Details3>
+          <BlockLogoTitle>
+            <LogoProjet src={oxalia} alt="image projet" />
+            <Title>Programme D’abord Jeune</Title>
+          </BlockLogoTitle>
+          <ContainerBlock>
+            <BlockUl>
+              <ContainerUl1>
+                <Test>
+                  <LiList>
+                    <Span>Porteur du projet :</Span>
+                    <p>Groupement Oxalia (bailleurs sociaux)</p>
+                  </LiList>
+                  <LiList>
+                  <Span>Enjeux :</Span>
+                    <p>
+                      Prévenir les sorties sèches à la fin des dispositifs
+                      institutionnels et permettre l’accès au l’autonomie
+                      citoyenne des jeunes
+                    </p>
+                  </LiList>
+                  <LiList>
+                  <Span>Territoires :</Span>
+                    <p>
+                      Hauts-de-France, Valenciennois, Métropole Européenne de
+                      LiListlle, Maubeugeois-Avesnois
+                    </p>
+                  </LiList>
+                  <LiList>
+                  <Span>Mission Favart :</Span>
+                    <p>
+                      Mise en œuvre d’une nouvelle stratégie régionale pour
+                      lutter contre les sorties sèches à la fin de l’aide
+                      sociale à l’enfance (ASE)
+                    </p>
+                  </LiList>
+                  <LiList>
+                    <OutilsContainer>
+                    <Span>Outils Favart :</Span>
+                      <Center>
+                        <Tools>Droit à l’expérimentation</Tools>
+                        <Tools>Communauté d’action</Tools>
+                        <Tools>Modèle opérationnel</Tools>
+                        <Tools>Modèle économique à impact</Tools>
+                        <Tools>Modèle juridique et financier</Tools>
+                      </Center>
+                    </OutilsContainer>
+                  </LiList>
+                </Test>
+              </ContainerUl1>
+            </BlockUl>
+          </ContainerBlock>
         </Grid3>
-        <Grid4>
-          <Title>Block4</Title>
-          <Details4>ribus eius nulla in, tenetur odio. Rerum.</Details4>
-        </Grid4>
-        <Grid5>
-          <Title>Block5</Title>
-          <Details5>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum alias
-            voluptates fugiat modi eveniet rerum vitae distinctio laudantium
-            porro labore dolore illum sapiente temporibus eius nulla in, tenetur
-            odio. Rerum.
-          </Details5>
-        </Grid5>
-        <Grid6>
-          <Title>Block6</Title>
-          <Details6>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum alias
-            voluptates fugiat modi eveniet rerum vitae distinctio laudantium
-            porro labore dolore illum sapiente temporibus eius nulla in, tenetur
-            odio. Rerum.
-          </Details6>
-        </Grid6>
       </Container>
     </>
   );
--- src/styled-components/Grid.jsx
+++ src/styled-components/Grid.jsx
@@ -3,13 +3,12 @@ import { Device } from "../assets/Device/Device";
 
 export const Container = styled.div`
   display: grid;
-  margin-left: 7vw;
-  margin-right: 7vw;
+  margin 2vh 7vw 0 7vw;
+  height:auto;
   border: 2px dotted black;
-  grid-template-columns: repeat(5, 1fr);
-  grid-template-rows: repeat(3, 1fr);
+  grid-template-columns: 1fr 2fr 1fr 2fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr;
   grid-gap: 1vw;
-
   @media ${Device.mobile} {
     display: flex;
     flex-direction: column;
@@ -21,7 +20,7 @@ export const Container = styled.div`
 `;
 
 export const BlockLogoTitle = styled.div`
-  margin: 1vh 2vw 4vh 2vw;
+  margin: 0.5vw 1vw 1vw 1vw;
 `;
 
 export const Title = styled.h2`
@@ -94,15 +93,148 @@ export const LiList = styled.li`
 export const ContainerBlock = styled.div``
 
 export const Grid1 = styled.div`
-  grid-column-start: 2;
+  grid-column: 2/3;
+  grid-row: 1/2;
+  height: 22vh;
+  background-color: #1f89d1;
+  transition: 0.2s ease;
+  overflow: hidden;
+  &:hover {
+    background: #e5e5e5;
+    grid-column: 1/4;
+    grid-row: 1/3;
+    height: auto;
+    z-index: 1;
+    transition: 0.2s ease;
+    ${Description} {
+      opacity: 1;
+      transition: opacity 0.3s ease;
+    }
+    ${Title} {
+      opacity: 1;
+      transition: opacity 0.3s ease;
+    }
+    ${BlockLogoTitle} {
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+    }
+    ${BlockUl} {
+      opacity: 1;
+    }
+    ${ContainerBlock} {
+      opacity: 1;
+    }
+    ${BlockDescription} {
+      opacity: 1;
+    }
+  }
+  @media ${Device.mobile} {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+export const Grid2 = styled.div`
+  grid-column-start: 4;
   grid-row-start: 1;
+  height: 22vh;
+  background-color: #1f89d1;
+  transition: 0.2s ease;
+  overflow: hidden;
+  &:hover {
+    background: #e5e5e5;
+
+    grid-column: 3/6;
+    grid-row: 1/3;
+    height: auto;
+    z-index: 1;
+    transition: 0.2s ease;
+    ${Description} {
+      opacity: 1;
+      transition: opacity 0.3s ease;
+    }
+    ${Title} {
+      opacity: 1;
+      transition: opacity 0.3s ease;
+    }
+    ${BlockLogoTitle} {
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+    }
+    ${BlockUl} {
+      opacity: 1;
+    }
+    ${ContainerBlock} {
+      opacity: 1;
+    }
+    ${BlockDescription} {
+      opacity: 1;
+    }
+  }
+  @media ${Device.mobile} {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+export const Grid3 = styled.div`
+  grid-column: 2/3;
+  grid-row: 2/3;
+  height: 22vh;
+  background-color: #1f89d1;
+  transition: 0.2s ease;
+  overflow: hidden;
+  &:hover {
+    background: #e5e5e5;
+    grid-column: 1/4;
+    grid-row: 2/6;
+    height: auto;
+    z-index: 1;
+    transition: 0.2s ease;
+    ${Description} {
+      opacity: 1;
+      transition: opacity 0.3s ease;
+    }
+    ${Title} {
+      opacity: 1;
+      transition: opacity 0.3s ease;
+    }
+    ${BlockLogoTitle} {
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+    }
+    ${BlockUl} {
+      opacity: 1;
+    }
+    ${ContainerBlock} {
+      opacity: 1;
+    }
+    ${BlockDescription} {
+      opacity: 1;
+    }
+  }
+  @media ${Device.mobile} {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+export const Grid4 = styled.div`
+  grid-column-start: 4;
+  grid-row-start: 2;
   height: 22vh;
   background-color: #1f89d1;
   transition: 0.2s ease;
   &:hover {
     background: #e5e5e5;
 
-    grid-column: 1/4;
+    grid-column: 3/6;
     grid-row: 1/3;
     height: auto;
     z-index: 1;
@@ -143,104 +275,6 @@ export const Test = styled.ul`
 
 export const Details2 = styled.p`
   opacity: 0;
-`;
-export const Grid2 = styled.div`
-  grid-column-start: 4;
-  grid-row-start: 1;
-  background: green;
-  color: white;
-  text-align: center;
-  &:hover {
-    grid-column: 3/6;
-    grid-row: 1/3;
-    z-index: 1;
-    ${Details2} {
-      opacity: 1;
-      transition: opacity 0.3s ease;
-    }
-  }
-`;
-export const Details3 = styled.p`
-  opacity: 0;
-`;
-
-export const Grid3 = styled.div`
-  grid-column-start: 2;
-  grid-row-start: 2;
-  background: blue;
-  color: white;
-  text-align: center;
-  &:hover {
-    grid-column: 1/4;
-    grid-row: 2/4;
-    z-index: 1;
-    ${Details3} {
-      opacity: 1;
-      transition: opacity 0.3s ease;
-    }
-  }
-`;
-
-export const Details4 = styled.p`
-  opacity: 0;
-`;
-
-export const Grid4 = styled.div`
-  grid-column-start: 4;
-  grid-row-start: 2;
-  background: gray;
-  color: white;
-  text-align: center;
-  &:hover {
-    grid-column: 3/6;
-    grid-row: 2/4;
-    z-index: 1;
-
-    ${Details4} {
-      opacity: 1;
-      transition: opacity 0.3s ease;
-    }
-  }
-`;
-export const Details5 = styled.p`
-  opacity: 0;
-`;
-
-export const Grid5 = styled.div`
-  grid-column-start: 2;
-  grid-row-start: 3;
-  background: purple;
-  color: white;
-  text-align: center;
-  &:hover {
-    grid-column: 1/4;
-    grid-row: 2/4;
-    z-index: 1;
-    ${Details5} {
-      opacity: 1;
-      transition: opacity 0.3s ease;
-    }
-  }
-`;
-
-export const Details6 = styled.p`
-  opacity: 0;
-`;
-export const Grid6 = styled.div`
-  grid-column-start: 4;
-  grid-row-start: 3;
-  background: orange;
-  color: white;
-  text-align: center;
-  &:hover {
-    grid-column: 3/6;
-    grid-row: 2/4;
-    z-index: 1;
-    ${Details6} {
-      opacity: 1;
-      transition: opacity 0.3s ease;
-    }
-  }
 `;
 
 export const Span = styled.span`
