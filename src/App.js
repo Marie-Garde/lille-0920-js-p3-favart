456227b840aaccf950a17c32ef8613a2c88efc52
Marie Garde <marie_garde@ymail.com>
2021-01-20 11:42:22 +0100

    database linked

--- src/App.js
+++ src/App.js
@@ -6,21 +6,14 @@ import Mainpage from "./components/Main";
 import Footer from "./components/Footer";
 import Agissons from "./components/Agissons";
 import Inscription from "./components/Inscription";
+import Link from "./components/Link";
 
 import { Switch, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <div>
-      <Reset />
-      <HeaderTop />
-      <Switch>
-        <Route exact path="/" component={Mainpage} />
-        <Route path="/agissons" component={Agissons} />
-        <Route path="/connexion" component={Connexion} />
-        <Route path="/inscription" component={Inscription} />
-      </Switch>
-      <Footer />
+      <Link />
     </div>
   );
 }
