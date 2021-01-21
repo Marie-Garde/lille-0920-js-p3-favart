c40e785bc38d6910d165c14d857501de3df1f17c
Gauthier-Lhomme <lhomme.gaut@gmail.com>
2021-01-21 14:08:53 +0100

    Add mail, phone, password and password confirmation div

--- src/components/Inscription.jsx
+++ src/components/Inscription.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { FormCheckbox } from "shards-react";
 
 import {
@@ -21,6 +22,36 @@ import {
 } from "../styled-components/Inscription";
 
 export default function Inscription() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    axios({
+      method: "POST",
+      url: `http://localhost:3001/client`,
+    }).then((res) => {
+      setClients(res.data);
+      setLoading(false);
+    });
+  }, [setClients]);
+
+
+  const [body,setBody] = useState({
+    name : "",
+    surname: "",
+    email: "",
+    phone : null,
+    password : "",
+    job : "",
+    structure_name: "",
+    structure_type :"",
+    structure_field : "",
+    job_field :"",
+    territory : "",
+    wishes:"",
+    website: "",
+  })
+
   return (
     <FlexBox>
       <FlexBox1></FlexBox1>
@@ -44,14 +75,14 @@ export default function Inscription() {
               <Label>Nom *</Label>
               <Input
                 type="text"
-                name="firstname"
-                id="firstname"
+                name="surname"
+                id="surname"
                 placeholder="Nom"
                 required
               />
             </DivLabel>
             <DivLabel>
-              <Label>Métier *</Label>
+              <Label>Email *</Label>
               <Input
                 type="email"
                 name="email"
@@ -61,11 +92,51 @@ export default function Inscription() {
               />
             </DivLabel>
             <DivLabel>
+              <Label>Téléphone *</Label>
+              <Input
+                type="number"
+                name="phone"
+                id="phone"
+                placeholder="Numéro de téléphone"
+                required
+              />
+            </DivLabel>
+            <DivLabel>
+              <Label>Mot de passe *</Label>
+              <Input
+                type="text"
+                name="password"
+                id="password"
+                placeholder="Mot de passe"
+                required
+              />
+            </DivLabel>
+            <DivLabel>
+              <Label>Mot de passe *</Label>
+              <Input
+                type="text"
+                name="password"
+                id="password"
+                placeholder="Confirmer le mot de passe"
+                required
+              />
+            </DivLabel>
+            <DivLabel>
+              <Label>Métier *</Label>
+              <Input
+                type="text"
+                name="job"
+                id="job"
+                placeholder="Métier"
+                required
+              />
+            </DivLabel>
+            <DivLabel>
               <Label>Nom de la structure *</Label>
               <Input
                 type="text"
-                name="Structure name"
-                id="Structurename"
+                name="structure_name"
+                id="structure_name"
                 placeholder="Nom de la structure"
                 required
               />
@@ -74,8 +145,8 @@ export default function Inscription() {
               <Label>Type de la structure *</Label>
               <Input
                 type="text"
-                name="Structure name"
-                id="Structurename"
+                name="structure_type :"
+                id="structure_type :"
                 placeholder="Type de la structure"
                 required
               />
@@ -84,8 +155,8 @@ export default function Inscription() {
               <Label>Domaine de la structure *</Label>
               <Input
                 type="text"
-                name="Structure name"
-                id="Structurename"
+                name="structure_field"
+                id="structure_field"
                 placeholder="Domaine de la structure"
                 required
               />
@@ -94,8 +165,8 @@ export default function Inscription() {
               <Label>Votre domaine d'activité</Label>
               <Textarea
                 type="text"
-                name="message"
-                id="message"
+                name="job_field"
+                id="job_field"
                 placeholder="Domaine d'activité"
               />
             </DivLabel>
@@ -113,8 +184,8 @@ export default function Inscription() {
               <Label>Autre, précisez</Label>
               <TextareaAutre
                 type="text"
-                name="precisez"
-                id="message"
+                name="territory"
+                id="territory"
                 placeholder="Autre, précisez"
               />
             </DivLabelText>
@@ -122,8 +193,8 @@ export default function Inscription() {
               <Label>Vos envies</Label>
               <TextareaEnvies
                 type="text"
-                name="envies"
-                id="message"
+                name="wishes"
+                id="wishes"
                 placeholder="Vos envies"
               />
             </DivLabelText>
@@ -131,8 +202,8 @@ export default function Inscription() {
               <Label>Votre site internet</Label>
               <InputSite
                 type="text"
-                name="Site internet"
-                id="SiteInternet"
+                name="website"
+                id="website"
                 placeholder="Votre site internet"
               />
             </DivLabelText>
