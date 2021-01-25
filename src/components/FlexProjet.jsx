cf90e41e60404a28ed3933ae5488e9bae6c60973
Fx-Colliot <fxcolliot@gmail.com>
2021-01-25 23:34:37 +0100

    Change grid to flexbox, projects disposition ok, still to do css hover and check responsive

--- src/components/FlexProjet.jsx
+++ src/components/FlexProjet.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from "react";
-import oxalia from "../assets/logosProjetsGrid/logo-oxalia.png";
-import chenelet from "../assets/logosProjetsGrid/FONCIERE_Logo-Couleur-vertical-1.png";
+
 import axios from "axios";
 import {
   FirstTitle,
   Container,
   TitleProject,
-  Grid1,
+  Flex,
   LogoProjet,
   BlockLogoTitle,
   BlockUl,
@@ -19,6 +18,8 @@ import {
   Span,
   Center,
   Page,
+  ContainerLogo,
+  FlexContainer,
 } from "../styled-components/FlexProjet";
 
 export default function FlexProjet() {
@@ -39,61 +40,65 @@ export default function FlexProjet() {
   return (
     <>
       <Page>
-        {projets.map((projet) => {
-          return (
-            <div key={projet.id}>
-              <FirstTitle>
-                S’autoriser le droit à l’expérimentation territoriale avec les
-                acteurs engagés
-              </FirstTitle>
-              <Container>
-                <Grid1>
-                  <BlockLogoTitle>
-                    <LogoProjet src={oxalia} alt="image projet" />
-                    <TitleProject>{projet.titre}</TitleProject>
-                  </BlockLogoTitle>
-                  <ContainerBlock>
-                    <BlockUl>
-                      <ContainerUl1>
-                        <Test>
-                          <LiList>
-                            <Span>Porteur du projet :</Span>
-                            <p>{projet.porteurs}</p>
-                          </LiList>
-                          <LiList>
-                            <Span>Enjeux :</Span>
-                            <p>{projet.enjeux}</p>
-                          </LiList>
-                          <LiList>
-                            <Span>Mission Favart :</Span>
-                            <p>{projet.missions}</p>
-                          </LiList>
-                          <LiList>
-                            <Span>Partenaires :</Span>
-                            <p>{projet.partenaires}</p>
-                          </LiList>
-                          <LiList>
-                            <Span>Territoires :</Span>
-                            <p>{projet.patenaires}</p>
-                          </LiList>
+        <FirstTitle>
+          S’autoriser le droit à l’expérimentation territoriale avec les acteurs
+          engagés
+        </FirstTitle>
+        <FlexContainer>
+          <Container>
+            {projets.map((projet) => {
+              return (
+                <div key={projet.id}>
+                  <Flex>
+                    <BlockLogoTitle>
+                      <ContainerLogo bk={projet.logo}>
+                        <LogoProjet /*src={projet.logo} alt="image projet"*/ />
+                      </ContainerLogo>
+                      <TitleProject>{projet.titre}</TitleProject>
+                    </BlockLogoTitle>
+                    <ContainerBlock>
+                      <BlockUl>
+                        <ContainerUl1>
+                          <Test>
+                            <LiList>
+                              <Span>Porteur du projet :</Span>
+                              <p>{projet.porteurs}</p>
+                            </LiList>
+                            <LiList>
+                              <Span>Enjeux :</Span>
+                              <p>{projet.enjeux}</p>
+                            </LiList>
+                            <LiList>
+                              <Span>Mission Favart :</Span>
+                              <p>{projet.missions}</p>
+                            </LiList>
+                            <LiList>
+                              <Span>Partenaires :</Span>
+                              <p>{projet.partenaires}</p>
+                            </LiList>
+                            <LiList>
+                              <Span>Territoires :</Span>
+                              <p>{projet.patenaires}</p>
+                            </LiList>
 
-                          <LiList>
-                            <OutilsContainer>
-                              <Span>Outils Favart :</Span>
-                              <Center>
-                                <Tools>{projet.outils}</Tools>
-                              </Center>
-                            </OutilsContainer>
-                          </LiList>
-                        </Test>
-                      </ContainerUl1>
-                    </BlockUl>
-                  </ContainerBlock>
-                </Grid1>
-              </Container>
-            </div>
-          );
-        })}
+                            <LiList>
+                              <OutilsContainer>
+                                <Span>Outils Favart :</Span>
+                                <Center>
+                                  <Tools>{projet.outils}</Tools>
+                                </Center>
+                              </OutilsContainer>
+                            </LiList>
+                          </Test>
+                        </ContainerUl1>
+                      </BlockUl>
+                    </ContainerBlock>
+                  </Flex>
+                </div>
+              );
+            })}
+          </Container>
+        </FlexContainer>
       </Page>
     </>
   );
