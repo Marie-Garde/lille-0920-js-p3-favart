ad66e171158c74557eb58cf766616e775e0aad66
Gauthier-Lhomme <lhomme.gaut@gmail.com>
2021-01-26 16:11:20 +0100

    ContactUs Page

---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import emailjs from 'emailjs-com';
+
+export default function ContactUs() {
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_q3jbdzm', 'template_stdceyl', e.target, 'user_NKYLcqZwiFms9AL3RjBZO')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+
+  return (
+    <form className="contact-form" onSubmit={sendEmail}>
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+}
--- src/components/ProfilPage.jsx
+++ src/components/ProfilPage.jsx
@@ -14,6 +14,7 @@ import {
   CardText,
   CardBdd,
 } from "../styled-components/ProfilPage.jsx";
+import ContactUs from "./ContactUs.jsx";
 
 export default () => {
   const [clients, setClients] = useState([]);
@@ -94,6 +95,9 @@ export default () => {
               </Card>
             </>
           )}
+        </div>
+        <div>
+          <ContactUs />
         </div>
       </FlexBox2>
     </FlexBox>
