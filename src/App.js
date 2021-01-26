f4f8feae03611f5e43408488a0bb9944b9851e03
Gauthier-Lhomme <lhomme.gaut@gmail.com>
2021-01-26 16:46:34 +0100

    Prettify

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import ClientPage from "./components/ClientPage";
 import { Reset } from "styled-reset";
 import Connexion from "./components/Connexion";
 import HeaderTop from "./components/HeaderTop";
-import Mainpage from "./components/Main";
-import Footer from "./components/Footer";
-import Agissons from "./components/Agissons";
-import Inscription from "./components/Inscription";
-
 import { Switch, Route } from "react-router-dom";
-import useForm from "./components/ConnexionLogin";
-import ProfilPage from "./components/ProfilPage";
-import Sidebar from "./components/Sidebar";
-
 export default function App() {
   const [token, setToken] = useState("");
   console.log(token);
@@ -21,7 +12,6 @@ export default function App() {
     <div>
       <Reset />
       <HeaderTop />
-      <Sidebar />
       <Switch>
         <Route path="/clientpage" component={ClientPage} />
         <Route exact path="/">
