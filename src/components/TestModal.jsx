279cecbcda354c79e111d9c0946e73d3ea60eb7b
Fx-Colliot <fxcolliot@gmail.com>
2021-01-30 15:15:23 +0100

    Css modal projets

--- src/components/TestModal.jsx
+++ src/components/TestModal.jsx
@@ -12,35 +12,53 @@ const Picture = styled.img`
   height: 30vh;
   width: auto;
 `;
+const Button = styled.button`
+  height: 20vh;
+  width: 20vw;
+`;
 
+const Image = styled.img`
+  max-height: 15vh;
+  max-width: 20vw;
+`;
 const Content = styled.div``;
 
-export default function DataTest() {
-    const [projets, setProjets] = useState([]);
-    const [isDisplay, setDisplay] = useState({ isDisplay: false, id: "" });
+const ContentProject = styled.div`
+  display: flex;
+  border: 2px solid black;
+  flex-direction: column;
+  align-items: center;
+  height: 70vh;
+  width: 80vw;
+  flex-wrap: wrap;
+  gap: 5vh;
+`;
 
-    useEffect(() => {
-        axios
-          .get("http://localhost:5050/projets")
-          .then(function (response) {
-            setProjets(response.data);
-            console.log(response.data);
-          })
-          .catch(function (error) {
-          });
-      },[]);
+export default function DataTest() {
+  const [projets, setProjets] = useState([]);
+  const [isDisplay, setDisplay] = useState({ isDisplay: false, id: "" });
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5050/projets")
+      .then(function (response) {
+        setProjets(response.data);
+        console.log(response.data);
+      })
+      .catch(function (error) {});
+  }, []);
 
   return (
-    <div>
+    <ContentProject>
       {projets.map((projet) => (
         <Content key={projet.id}>
-          <button
+          <Button
             onClick={() =>
               setDisplay({ isDisplay: !isDisplay.isDisplay, id: projet.id })
             }
           >
-            {projet.id}
-          </button>
+            <Image src={projet.logo} />
+          </Button>
           <PictureContent
             onClick={() => console.log(projet.id)}
             className={
@@ -58,9 +76,8 @@ export default function DataTest() {
             <p>{projet.missions}</p>
             <p>{projet.outils}</p>
           </PictureContent>
-          .
         </Content>
       ))}
-    </div>
+    </ContentProject>
   );
 }
