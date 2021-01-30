eb044e4f1f78d041824ef1fd137eae372a038340
Fx-Colliot <fxcolliot@gmail.com>
2021-01-31 00:06:58 +0100

    Work on responsive projects

--- src/components/TestModal.jsx
+++ src/components/TestModal.jsx
@@ -1,33 +1,48 @@
 import styled from "styled-components";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import { Device } from "../assets/Device/Device";
 
 const PictureContent = styled.div`
-    position: relative;
-    top: -180px;
-    left: 380px;
-    height:0px;
+  position: relative;
+  top: -180px;
+  left: 380px;
+  height: 0px;
   &.hide {
     display: none;
+  }
+  @media ${Device.mobile} {
+    top: 0;
+    left: 0;
+    width: 80vw;
   }
 `;
 
 const Picture = styled.img`
   height: 25vh;
   width: auto;
+  @media ${Device.mobile} {
+    height: 20vh;
+    width: 60vw;
+  }
 `;
 const Button = styled.button`
   height: 20vh;
   width: 20vw;
+  cursor: pointer;
+  @media ${Device.mobile} {
+    width: 50vw;
+    margin-left: 20vw;
+  }
 `;
 
 const Flex = styled.div`
   display: flex;
-  overflow:hidden;
+  overflow: hidden;
   flex-direction: column;
   gap: 3vh;
   margin: 10vh 6vw 0 6vw;
-  height:190vh;
+  height: 190vh;
 
   h1 {
     font-size: 2rem;
@@ -38,8 +53,8 @@ const Flex = styled.div`
 const H3 = styled.h3`
   font-size: 1.5rem;
   font-weight: bold;
-  margin-top:2vh;
-  text-decoration:underline;
+  margin-top: 2vh;
+  text-decoration: underline;
 `;
 
 const LogoTitle = styled.div`
@@ -50,29 +65,41 @@ const LogoTitle = styled.div`
   margin: 3vh 3vw 5vh 3vw;
 
   h1 {
-    color:#fff;
-    font-size:2.5rem;
+    color: #fff;
+    font-size: 2.5rem;
+  }
+  @media ${Device.mobile} {
+    flex-direction: column;
+    h1 {
+      color: #fff;
+      font-size: 2rem;
+    }
   }
 `;
 
 const Container = styled.div`
   border: 1px solid black;
-  border-radius:15px;
-  background-color:#122239;
-  width:60vw;
+  border-radius: 15px;
+  background-color: #122239;
+  width: 60vw;
+  @media ${Device.mobile} {
+    width: 90vw;
+  }
 `;
 
 const Content = styled.div`
   margin: 0 3vw 3vh 3vw;
-  color:#fff;
+  color: #fff;
 `;
 
 const Image = styled.img`
   max-height: 15vh;
   max-width: 20vw;
+  @media ${Device.mobile} {
+    max-height: 25vh;
+    max-width: 25vw;
+  }
 `;
-
-
 
 export default function DataTest() {
   const [projets, setProjets] = useState([]);
@@ -98,7 +125,7 @@ export default function DataTest() {
                 setDisplay({ isDisplay: !isDisplay.isDisplay, id: projet.id })
               }
             >
-             <Image src={projet.logo} />
+              <Image src={projet.logo} />
             </Button>
             <PictureContent
               onClick={() => console.log(projet.id)}
