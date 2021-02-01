adf8f4b75f8f48a02899f9ae9a7536eb574a0813
Marie Garde <marie_garde@ymail.com>
2021-02-01 09:32:57 +0100

    Prettify

---
+++ src/components/ConnexionLogin.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+
+export default function useForm({ setToken }) {
+  let history = useHistory();
+
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:3001/auth/login", values).then((res) => {
+      setToken(res.data);
+      history.push("/clientpage");
+    });
+  };
+
+  return { handleChange, values, handleSubmit };
+}
---
+++ src/components/InscriptionSignUp.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+
+export default function useForm(validateInfo) {
+  let history = useHistory();
+
+  const [values, setValues] = useState({
+    username: "",
+    surname: "",
+    email: "",
+    phone: "",
+    password: "",
+    password2: "",
+    job: "",
+    structure_name: "",
+    structure_type: "",
+    structure_field: "",
+    job_field: "",
+    territory: "",
+    wishes: "",
+    website: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors(validateInfo(values));
+    delete values.password2;
+    axios.post("http://localhost:3001/auth/signup", values).then((res) => {
+      setValues(res.data);
+      history.push("/clientpage");
+    });
+  };
+
+  return { handleChange, values, handleSubmit, errors };
+}
